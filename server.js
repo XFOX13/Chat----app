aaced0e7bac3623f6ebbd9d85bb221a0a02fc83e
XFOX13 <muhammedyutube64@gmail.com>
2025-06-22 16:47:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const { Server } = require("socket.io");
 const io = new Server(3000, {
-  cors: { origin: "*" } // Allow all origins (for testing)
+  cors: { origin: "*" }
 });
 
 io.on("connection", socket => {
   console.log("A user connected");
 
   socket.on("chat-message", data => {
-    // Broadcast message to everyone except sender
-    socket.broadcast.emit("chat-message", data);
+    io.emit("chat-message", data); // Send to all users including sender
   });
 
   socket.on("disconnect", () => {
